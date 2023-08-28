69ae83b2b62a0f32be05f6689cbabbe77e4bd02d
n-sangsasitorn <90594512+n-sangsasitorn@users.noreply.github.com>
2023-08-28 15:01:48 +0900

    Update 3sendInfotoSplunk.sh

--- 3sendInfotoSplunk.sh
+++ 3sendInfotoSplunk.sh
@@ -11,8 +11,10 @@
 #
 # The settings you need to set:
 # 1. splunkHECURL (Splunk URL), dashboardHECtoken (Main HEC token), endingHECtoken (Separate HEC token for notifying Slack)
-# 2. We convert time to JST(Japan Standard Time) which is UTC+9. 
-# If you would like to convert to your timezone, you can change Number of seconds added on line 61. (Number of seconds=Number of hours differ from UTC*60*60)
+# 2. From which point of time you would like to send the AfterMath data to splunk (for example, 9 hours or 1 day from NOW ), you can change on line 59.
+# Default setting is collecting information from 9 hours from now.
+# 3. Due to this issue (https://github.com/jamf/aftermath/issues/49), We convert time of Syslog events to JST(Japan Standard Time) which is UTC+9. 
+# If you would like to convert to your timezone, you can change Number of seconds added on line 71. (Number of seconds=Number of hours differ from UTC*60*60)
 #===================================
 
 # Clean up Aftermath file and log
@@ -44,44 +46,49 @@ echo $aftermathTimeline
 
 #=========================================================
 # Set point of time of the events you'd like to send to Splunk (for example, send the event occurred 9 hour before until NOW )
-# Convert the event time to local timezone (JST) 
-# Compose the json file from the event data and add occurence time, serial number, and host name info to the event's json file
+# Convert Syslog events' time to local timezone (JST) 
+# Compose the json file from the event data and add occurence time (in local timezone), serial number, and host name info to the event's json file
 
 hostName=$(scutil --get ComputerName)
 splunkHECURL=""
 endingHECtoken=""
 dashboardHECtoken=""
 
-#From which point of time do you like to send the data to splunk (for example, 9 hour or 1 day from NOW )
-#now=$(date -v-1d +"%Y-%m-%dT%H:%M:%SZ")
-now=$(date -v -9H +"%Y-%m-%dT%H:%M:%SZ")
+#Set point of time of the events you'd like to send to Splunk (for example, 9 hours or 1 day from NOW )
+#now=$(date -v-1d +"%Y-%m-%dT%H:%M:%SZ") # 1 day from NOW
+now=$(date -v -9H +"%Y-%m-%dT%H:%M:%SZ") # 9 hours from NOW
 echo $now
 timestamp1=$(date -j -f "%Y-%m-%dT%H:%M:%SZ" "$now" "+%s")
 
 while IFS='' read -r line || [ "$line" ]
 do
 	originalDate=$(echo "$line" | awk -F ',' '{print $1}') 
+	category=$(echo "$line" | awk -F ',' '{print $2}') 
 	echo "OriginalDateUTC= $originalDate" >> /private/tmp/aftermath_temp.txt
 	originalDateEpoch=$(date -j -f "%Y-%m-%dT%H:%M:%SZ" $originalDate +"%s" )
-	originalDateEpochJST=$(( $originalDateEpoch+32400 ))
+	if [[ $category != *"SYSLOG"* ]]
+	then
+		originalDateEpochJST=$(( $originalDateEpoch+32400 ))
+	else
+		originalDateEpochJST=$originalDateEpoch
+	fi
 	originalDateJST=$(date -r $originalDateEpochJST +"%Y-%m-%dT%H:%M:%SZ")
 	
-	#In order to notify slack, we set separate HEC for sending the end message. 
- 	#Once Splunk got the message sent, it will notify Slack channel.
-	if [[ "$timestamp1" -gt "$originalDateEpochJST" ]]; then
-    	json="{\"event\": \"ENDING MESSAGE\", \"serialNumber\": \"$serialNumber\", \"hostName\": \"$hostName\"}"
-        curl ${splunkHECURL}/services/collector/raw -H "Authorization: Splunk ${endingHECtoken}" -d "$json"
+	#In order to notify slack, we set separate HEC for sending the end message. Once Splunk got this message, it will notify Slack channel.
+	if [[ $category != *"SYSLOG"* && "$timestamp1" -gt "$originalDateEpochJST" ]]; then
+		json="{\"event\": \"ENDING MESSAGE\", \"serialNumber\": \"$serialNumber\", \"hostName\": \"$hostName\"}"
+		curl ${splunkHECURL}/services/collector/raw -H "Authorization: Splunk ${endingHECtoken}" -d "$json"
 		break;
+	elif [[ "$timestamp1" -le "$originalDateEpochJST" ]]; then
+		json=$(echo "$line" | awk -F ',' '{printf "{ \"category\": \"%s\", \"event\": \"%s\"}\n", $2, $3}')
+		hostInfo=", \"serialNumber\": \"$serialNumber\", \"hostName\": \"$hostName\"}"
+		hostInfo1="{\"originalDate\": \"$originalDateJST\","
+		json=$(echo $json | sed "s/}/$hostInfo/g")
+		json=$(echo $json | sed "s/{/$hostInfo1/g")
+		echo $json >> /private/tmp/aftermath_temp.txt
+			
+		# Sending the information to Splunk Analysis Dashboard
+		curl ${splunkHECURL}/services/collector/raw -H "Authorization: Splunk ${dashboardHECtoken}" -d "$json"
 	fi
-	
-	json=$(echo "$line" | awk -F ',' '{printf "{ \"category\": \"%s\", \"event\": \"%s\"}\n", $2, $3}')
-	hostInfo=", \"serialNumber\": \"$serialNumber\", \"hostName\": \"$hostName\"}"
-	hostInfo1="{\"originalDate\": \"$originalDateJST\","
-	json=$(echo $json | sed "s/}/$hostInfo/g")
-	json=$(echo $json | sed "s/{/$hostInfo1/g")
-	echo $json >> /private/tmp/aftermath_temp.txt
-	
-	# Sending the information to Splunk Analysis Dashboard
-	curl ${splunkHECURL}/services/collector/raw -H "Authorization: Splunk ${dashboardHECtoken}" -d "$json"
 	
 done < ${aftermathTimeline}
