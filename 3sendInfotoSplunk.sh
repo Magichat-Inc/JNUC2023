48b05bf7d3171239d70605afa50e85825d293916
n-sangsasitorn <90594512+n-sangsasitorn@users.noreply.github.com>
2023-08-28 23:08:38 +0900

    Update 3sendInfotoSplunk.sh

--- 3sendInfotoSplunk.sh
+++ 3sendInfotoSplunk.sh
@@ -12,9 +12,9 @@
 # The settings you need to set:
 # 1. splunkHECURL (Splunk URL), dashboardHECtoken (Main HEC token), endingHECtoken (Separate HEC token for notifying Slack)
 # 2. From which point of time you would like to send the AfterMath data to splunk (for example, 9 hours or 1 day from NOW )
-# Default setting is collecting information from 9 hours from now. You can change the setting on line 59. 
+# Default setting is collecting information from 9 hours from now. You can change the setting on line 58. 
 # 3. Due to this issue (https://github.com/jamf/aftermath/issues/49), We convert time of Syslog events to JST(Japan Standard Time) which is UTC+9. 
-# If you would like to convert to your timezone, you can change Number of seconds added on line 71. (Number of seconds=Number of hours differ from UTC*60*60)
+# If you would like to convert to your timezone, you can change Number of seconds added on line 75. (Number of seconds=Number of hours differ from UTC*60*60)
 #===================================
 
 # Clean up Aftermath file and log
@@ -45,21 +45,25 @@ aftermathTimeline="/private/tmp/Aftermath_Analysis_${serialNumber}/storyline.csv
 echo $aftermathTimeline
 
 #=========================================================
-# Set point of time of the events you'd like to send to Splunk (for example, send the event occurred 9 hour before until NOW )
-# Convert Syslog events' time to local timezone (JST) 
-# Compose the json file from the event data and add occurence time (in local timezone), serial number, and host name info to the event's json file
 
+# Specify all the necessary variables 
 hostName=$(scutil --get ComputerName)
 splunkHECURL=""
 endingHECtoken=""
 dashboardHECtoken=""
 
-#Set point of time of the events you'd like to send to Splunk (for example, 9 hours or 1 day from NOW )
-#now=$(date -v-1d +"%Y-%m-%dT%H:%M:%SZ") # 1 day from NOW
+# Set point of time of the events you'd like to send to Splunk (for example, 9 hours or 1 day from NOW )
+# Convert the point of time to Epoch format
+# now=$(date -v-1d +"%Y-%m-%dT%H:%M:%SZ") # 1 day from NOW
 now=$(date -v -9H +"%Y-%m-%dT%H:%M:%SZ") # 9 hours from NOW
 echo $now
 timestamp1=$(date -j -f "%Y-%m-%dT%H:%M:%SZ" "$now" "+%s")
 
+# Read each line from storyline.csv, then extract the values from each line.
+# If category is SYSLOG, then convert date&time to local timezone (JST) 
+# Event time will be compared to the point of time you would like to obtain the data set earlier on line 58
+# If event time is happened before the point of time you would like to obtain the data, then it will stop processing and send the end message to Splunk.
+# Otherwise, it continues to send the data to Splunk in json format.
 while IFS='' read -r line || [ "$line" ]
 do
 	originalDate=$(echo "$line" | awk -F ',' '{print $1}') 
@@ -74,7 +78,8 @@ do
 	fi
 	originalDateJST=$(date -r $originalDateEpochJST +"%Y-%m-%dT%H:%M:%SZ")
 	
-	#In order to notify slack, we set separate HEC for sending the end message. Once Splunk got this message, it will notify Slack channel.
+  	# In order to notify Slack, we set separate HTTP Endpoint Collection (HEC) for sending the end message. 
+ 	# Once Splunk receives this message, it will notify in specified Slack channel.
 	if [[ $category != *"SYSLOG"* && "$timestamp1" -gt "$originalDateEpochJST" ]]; then
 		json="{\"event\": \"ENDING MESSAGE\", \"serialNumber\": \"$serialNumber\", \"hostName\": \"$hostName\"}"
 		curl ${splunkHECURL}/services/collector/raw -H "Authorization: Splunk ${endingHECtoken}" -d "$json"
